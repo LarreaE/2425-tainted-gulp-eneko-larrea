bb531d04096339285608fc0c00b33d372f6df773
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-10-09 11:31:53 +0200

    [ADD] added PotionBag

--- PotionBag.mjs
+++ PotionBag.mjs
@@ -7,6 +7,35 @@ class PotionBag {
 
     static create(ingredients,cauldron)
     {
+        console.log(ingredients);
+        
+        let potions = [];
+        
+        for (const key in ingredients) {
+            console.log(ingredients[key]);
+            
+            for (const j in ingredients) {
+                
+                const potion = cauldron.createPotion(ingredients[key],ingredients[j]);
+                if (potion.name != 'Failed Potion' && !isInPouch(potions,potion.name)) {
+                    potions.push(potion);
+                }
+            }
+        }
+
+        console.log(potions);
         
     }
 }
+
+function isInPouch(pouch,potion_name)
+{
+    for (let i = 0; i < pouch.length; i++) {
+        if (pouch[i].name === potion_name) {
+            return true
+        }        
+    }
+    return false
+}
+
+export default PotionBag
--- index.mjs
+++ index.mjs
@@ -2,11 +2,13 @@
 
 import Cauldron from "./Cauldron.mjs";
 import Ingredients from "./Ingredients.mjs";
-import { getIngredientsData } from "./service.mjs";
+import PotionBag from "./PotionBag.mjs";
+import { getBagData, getIngredientsData } from "./service.mjs";
 
 const execute = async () => {
     try {
         const data = await getIngredientsData();
+        const bagsData = await getBagData();
 
         const ingredients = Ingredients.load(data)
 
@@ -15,7 +17,7 @@ const execute = async () => {
         //potion creation
         const potion1 = cauldron.createPotion('Bear Claws','Bee');
         const potion2 = cauldron.createPotion("Chicken's Egg",'Chaurus Eggs');
-        const potion3 = cauldron.createPotion('Chaurus Eggs','Bleeding Crown');
+        const potion3 = cauldron.createPotion('Chaurus Eggs','Chaurus Eggs');
         const potion4 = cauldron.createPotion('Nightshade','Ectoplasm');
 
         showPotion(potion1)
@@ -23,9 +25,11 @@ const execute = async () => {
         showPotion(potion3)
         showPotion(potion4)
 
+        
+        const potionBag = PotionBag.create(bagsData.players[0].pouch_red,cauldron)
 
-
-
+        console.log(potionBag);
+        
     } catch (error) {
         console.log(error);
     }
